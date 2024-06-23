b389ab2585b24c7d77ff3424d0a04fa6323a40fe
Anthony030222002 <albieri.00212@gmail.com>
2024-06-23 13:14:58 -0500

    static

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+if 'RAILWAY_STATIC_URL' in os.environ:
+    STATIC_URL = os.environ['RAILWAY_STATIC_URL']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
